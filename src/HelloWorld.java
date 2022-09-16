867acde1f0a7fb47d93f0cba483e19c3f9dce9d4
Kletzenbauer <markus.kletzenbauer@uni-graz.at>
2022-09-16 19:06:55 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Welcome to KONF 2022!");
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4"); //STRG+D für Dublicate
+
+
+    }
+
+}
